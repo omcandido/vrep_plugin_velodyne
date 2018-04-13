c2fb96935f143c7bc69d3a520188ae335741f514
lockdpwn <gyurse@gmail.com>
2018-04-13 19:35:59 +0900

    tf tree name changed

--- src/velodyneROSModel.cpp
+++ src/velodyneROSModel.cpp
@@ -40,7 +40,7 @@ CVelodyneROSModel::CVelodyneROSModel(const int visionSensorHandles[4],float freq
     _pubVelodyne =ROS_server::getPublisher();
 
     //initialize the fixed fields of the output PointCloud2 message
-    _buffer.header.frame_id="odom";
+    _buffer.header.frame_id="velodyne_link"; // odom ==> velodyne_link
     _buffer.height=1; //unordered data
     _buffer.fields.resize(4); //convert x/y/z/intensity to fields
     _buffer.fields[0].name = "x"; _buffer.fields[1].name = "y"; _buffer.fields[2].name = "z"; _buffer.fields[3].name = "intensity";
